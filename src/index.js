144c43659365ace45da35ddd275dd4d6bc14dd15
jeslinms111-cmyk <jeslin.m.s111@kalvium.community>
2025-11-12 12:57:22 +0530

    feat: setup initial project structure and http server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Blogify API is running!");
+  res.end("Server is running!");
 });
 
 const PORT = 3000;
